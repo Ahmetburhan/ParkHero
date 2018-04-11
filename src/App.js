e00f56be6e8ebf8082bab926c981d599f4bbc98c
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-04-11 15:52:32 -0700

    name and images

--- src/App.js
+++ src/App.js
@@ -6,110 +6,95 @@ import request from 'superagent';
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
-  state = {places:[]}
+  state = { places: [] }
 
-  componentDidMount () {
+  componentDidMount() {
 
 
-  // http://api.parkwhiz.com/parking/arlington/601-stadium-dr/?start=1297027800&end=1297047600&key=62d882d8cfe5680004fa849286b6ce20
+    // http://api.parkwhiz.com/parking/arlington/601-stadium-dr/?start=1297027800&end=1297047600&key=62d882d8cfe5680004fa849286b6ce20
 
-  //http://api.parkwhiz.com/search/?destination=312+N+wacker+Dr,+Chicago&start=1523403449&end=1523414249&key=62d882d8cfe5680004fa849286b6ce20
+    //http://api.parkwhiz.com/search/?destination=312+N+wacker+Dr,+Chicago&start=1523403449&end=1523414249&key=62d882d8cfe5680004fa849286b6ce20
 
-  request 
-    .get('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20').then(res => {
-      if(res.ok){
-        console.log(res.body)
-        this.setState({
-          places: res.body
-        })
-      }
-      else{
-        this.setState('Opps we found nothing!')
-        console.log('We found nothing')
-      }
-    })
+    request
+      .get('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20').then(res => {
+        if (res.ok) {
+          console.log(res.body)
+          this.setState({
+            places: res.body
+          })
+        }
+        else {
+          this.setState('Opps we found nothing!')
+          console.log('We found nothing')
+        }
+      })
     // .catch(err => console.log(err))
   };
 
-// componentWillMount() {
+  // componentWillMount() {
 
 
-//   // http://api.parkwhiz.com/parking/arlington/601-stadium-dr/?start=1297027800&end=1297047600&key=62d882d8cfe5680004fa849286b6ce20
+  //   // http://api.parkwhiz.com/parking/arlington/601-stadium-dr/?start=1297027800&end=1297047600&key=62d882d8cfe5680004fa849286b6ce20
 
-//   //http://api.parkwhiz.com/search/?destination=312+N+wacker+Dr,+Chicago&start=1523403449&end=1523414249&key=62d882d8cfe5680004fa849286b6ce20
+  //   //http://api.parkwhiz.com/search/?destination=312+N+wacker+Dr,+Chicago&start=1523403449&end=1523414249&key=62d882d8cfe5680004fa849286b6ce20
 
-//   request 
-//     .get('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20').then(res => {
-//       if(res.ok){
-//         console.log(res.body)
-//         this.setState({
-//           place: res.body
-          
-//         })
-//       }
-//       else{
-//         this.setState('Opps we found nothing!')
-//         console.log('We found nothing')
-//       }
-//     })
-//     .catch(err => console.log(err))
+  //   request 
+  //     .get('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20').then(res => {
+  //       if(res.ok){
+  //         console.log(res.body)
+  //         this.setState({
+  //           place: res.body
 
-//   // fetch('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20' )
-//   // .then ( response => response.json() )
-//   // // .then ( ({results: places}) => this.setState({places}))
+  //         })
+  //       }
+  //       else{
+  //         this.setState('Opps we found nothing!')
+  //         console.log('We found nothing')
+  //       }
+  //     })
+  //     .catch(err => console.log(err))
 
-// }
-render() {
-  let place = this.state.places
-  // console.log(place)
+  //   // fetch('http://api.parkwhiz.com/v4/quotes/?q=coordinates:41.8857256,-87.6369590&start_time=2018-04-23T12:00&end_time=2018-04-23T20:00&api_key=62d882d8cfe5680004fa849286b6ce20' )
+  //   // .then ( response => response.json() )
+  //   // // .then ( ({results: places}) => this.setState({places}))
+
+  // }
+  render() {
+    let place = this.state.places
+    // console.log(place)
     return (
 
-      
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          < h1 className = "App-title" > {
+          < h1 className="App-title" > {
             // place._embedded["pw:location"].name
-            } </h1>
+          } </h1>
         </header>
-        <p className="Place-intro">
+        <h3 className="Place-intro">
           <ul className="place-group">
             {place.map(place => (
               <p key={place._embedded["pw:location"].id}>
-                  {place._embedded["pw:location"].name}
+                {place._embedded["pw:location"].name}
               </p>
             ))}
-            {/* < img key = {place._embedded["pw:location"].id
-            } */}
-            {/* src = {
-                place._embedded["pw:location"].photos["0"].sizes.hub_frontpage.URL
-            }
-            alt = {
-             place._embedded["pw:location"].id
-            }
-            /> */}
+            {place.map(place => (
+              <img key={place._embedded["pw:location"].id}
+            className="user-info__avatar" src={place._embedded["pw:location"].photos["0"].sizes.hub_frontpage.URL} alt={`${place._embedded["pw:location"].id} avatar`} />
+             ))}
           </ul>
-        </p>
-      </div>
+        </h3>
+
+        
+
+
+          </div>
     );
   }
 }
 
 export default App;
-
-
-
-  // < ul className = "list-group-item"
-  // key = {
-  //     place._embedded["pw:location"].id
-  //   } >
-
-  //   <
-  //   img className = "img-responsive"
-  // src = {
-  //   place._embedded["pw:location"].photos["0"].sizes.hub_frontpage.URL
-  // }
-  // />
