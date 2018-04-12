204f0b761b994fb32682aaaf83ba345f45772de1
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-04-12 03:39:33 -0700

    api ifram map done

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ export default class Map extends React.Component {
         // if (!this.props.loaded) return <div>Loading...</div>;
 
         return (
-            <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+            <Iframe url="https://www.google.com/maps/embed/v1/streetview?location=37.7913,-122.3936&key=AIzaSyAjxQyFojuSIOq57lXHmAUbLjG44PYIoAE"
                 width="100%"
                 height="300px"
                 id="myId"
