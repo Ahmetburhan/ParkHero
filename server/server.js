d69d9d898f240e8c247ed754bd19005a6ae953c8
Anastasia Fefilova <fefilovaanastasiya@gmail.com>
2018-04-24 22:33:29 -0700

    server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+module.exports = app;
